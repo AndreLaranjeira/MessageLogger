7497f21e219183753798248fb5768269b0dd819e
André Laranjeira <AndreLaranjeira@protonmail.com>
2021-10-19 16:10:11 -0300

    Documentation for module's header file * Properly documented all the macros, enums and structs provided by the Message Logger module in it's header file;

--- include/message_logger.h
+++ include/message_logger.h
@@ -1,7 +1,13 @@
 // Copyright (c) 2019 André Filipe Caldas Laranjeira
 // MIT License
 
-// Message logger module - Header file.
+// Message Logger module - Header file.
+//! \file message_logger.h
+//! \author André Filipe Caldas Laranjeira
+//! \brief Message Logger module - Header file.
+//!
+//! The Message Logger module header file contains all macros, enumerations,
+//! types and function headers provided by the Message Logger module.
 
 // Define guard:
 #ifndef MESSAGE_LOGGER_H_
@@ -16,6 +22,20 @@
 #include <time.h>
 
 // Macros:
+
+//! \def DEFAULT_LOGGER_MESSAGE_COLORS
+//! \brief Macro to initialize a DisplayColors array with the default values
+//! used by the Message Logger for messages.
+//!
+//! The array's length is expected to be equal to #NUM_OF_MESSAGE_CATEGORIES.
+//!
+//! Usage example:
+//! \code
+//! LoggerColorPallet color_pallet = {
+//!   .message_colors = DEFAULT_LOGGER_MESSAGE_COLORS,
+//!   // Initialize other LoggerColorPallet members...
+//! };
+//! \endcode
 #define DEFAULT_LOGGER_MESSAGE_COLORS { \
   [DEFAULT_MSG] = {                     \
     .text_color = DFLT,                 \
@@ -38,6 +58,20 @@
     .background_color = DFLT            \
   }                                     \
 }
+
+//! \def DEFAULT_LOGGER_TAG_COLORS
+//! \brief Macro to initialize a DisplayColors array with the default values
+//! used by the Message Logger for message tags.
+//!
+//! The array's length is expected to be equal to #NUM_OF_TAG_CATEGORIES.
+//!
+//! Usage example:
+//! \code
+//! LoggerColorPallet color_pallet = {
+//!   .tag_colors = DEFAULT_LOGGER_TAG_COLORS,
+//!   // Initialize other LoggerColorPallet members...
+//! };
+//! \endcode
 #define DEFAULT_LOGGER_TAG_COLORS { \
   [CONTEXT_TAG] = {                 \
     .text_color = B_WHT,            \
@@ -60,66 +94,155 @@
     .background_color = DFLT        \
   }                                 \
 }
+
+//! \def TIME_FMT_SIZE
+//! \brief Char length of the string_representation member of a TimeFormat.
+//!
+//! Use this macro when storing a time format string representation outside of
+//! a TimeFormat variable or when copying a string representation to a
+//! TimeFormat variable.
 #define TIME_FMT_SIZE 50
 
 // Enumerations:
+
+//! \enum Color
+//! \brief A color supported for display by a terminal.
+//!
+//! Represents a color that can be displayed by a terminal via an ANSI escape
+//! code. Included are the 8 standard colors, their bright variants and the
+//! default color used by a given terminal.
 typedef enum {
-  BLA,
-  RED,
-  GRN,
-  YEL,
-  BLU,
-  MAG,
-  CYN,
-  WHT,
-  B_BLA,
-  B_RED,
-  B_GRN,
-  B_YEL,
-  B_BLU,
-  B_MAG,
-  B_CYN,
-  B_WHT,
-  DFLT
+  BLA,          //!< Black.
+  RED,          //!< Red.
+  GRN,          //!< Green.
+  YEL,          //!< Yellow.
+  BLU,          //!< Blue.
+  MAG,          //!< Magenta.
+  CYN,          //!< Cyan.
+  WHT,          //!< White.
+  B_BLA,        //!< Bright black.
+  B_RED,        //!< Bright red.
+  B_GRN,        //!< Bright green.
+  B_YEL,        //!< Bright yellow.
+  B_BLU,        //!< Bright blue.
+  B_MAG,        //!< Bright magenta.
+  B_CYN,        //!< Bright cyan.
+  B_WHT,        //!< Bright white.
+  DFLT          //!< Default color according to terminal settings.
 } Color;
 
+//! \enum LogFileMode
+//! \brief A file mode used to open a log file.
+//!
+//! Similarly to the mode argument passed when opening any regular file, this
+//! enumeration determines the behavior taken when a file with the same name as
+//! the one you are trying to open already exists.
 typedef enum {
-  WRITE,            // Write to log file.
-  APPEND            // Append to log file.
+  WRITE,        //!< Write to log file, overwriting any existing file.
+  APPEND        //!< Append to log file, assuming one already exists.
 } LogFileMode;
 
+//! \enum MessageCategory
+//! \brief A category of message supported by the Message Logger.
+//!
+//! Includes any and every different message type that is recognized by the
+//! Message Logger module, including the default message. The number of
+//! elements in this enumeration is stored in the #NUM_OF_MESSAGE_CATEGORIES
+//! companion macro.
 typedef enum {
-  DEFAULT_MSG,
-  ERROR_MSG,
-  INFO_MSG,
-  SUCCESS_MSG,
-  WARNING_MSG
+  DEFAULT_MSG,        //!< Default message. No further significance.
+  ERROR_MSG,          //!< Error message. Signifies an untreatable error.
+  INFO_MSG,           //!< Info message. Provides complementary information.
+  SUCCESS_MSG,        //!< Success message. Signifies a successful task.
+  WARNING_MSG         //!< Warning message. Signifies a treatable error.
 } MessageCategory;
 
+//! \enum TagCategory
+//! \brief A category of message tag supported by the Message Logger.
+//!
+//! Includes any and every different message tag type that is recognized by the
+//! Message Logger module, including the context tag. The number of
+//! elements in this enumeration is stored in the #NUM_OF_TAG_CATEGORIES
+//! companion macro.
 typedef enum {
-  CONTEXT_TAG,
-  ERROR_TAG,
-  INFO_TAG,
-  SUCCESS_TAG,
-  WARNING_TAG
+  CONTEXT_TAG,  //!< Context tag. Adds call context. Used by all message types.
+  ERROR_TAG,    //!< Error tag. Prefixes error messages.
+  INFO_TAG,     //!< Info tag. Prefixes info messages.
+  SUCCESS_TAG,  //!< Success tag. Prefixes success messages.
+  WARNING_TAG   //!< Warning tag. Prefixes warning messages.
 } TagCategory;
 
 // Enumeration companion macros:
+
+//! \def NUM_OF_MESSAGE_CATEGORIES
+//! \brief Number of message categories supported by the Message Logger.
+//!
+//! Companion macro to the #MessageCategory enumeration. Use it as the size of
+//! an array that should be accessed with a #MessageCategory as an index.
+//!
+//! Usage example:
+//! \code
+//! unsigned int message_log_quantity[NUM_OF_MESSAGE_CATEGORIES];
+//! message_log_quantity[DEFAULT_MSG] = 0;
+//! \endcode
 #define NUM_OF_MESSAGE_CATEGORIES 5
+
+//! \def NUM_OF_TAG_CATEGORIES
+//! \brief Number of message tag categories supported by the Message Logger.
+//!
+//! Companion macro to the #TagCategory enumeration. Use it as the size of an
+//! array that should be accessed with a #TagCategory as an index.
+//!
+//! Usage example:
+//! \code
+//! char tag_text[NUM_OF_TAG_CATEGORIES][100];
+//! strcpy(tag_text[ERROR_TAG], "Critical error");
+//! \endcode
 #define NUM_OF_TAG_CATEGORIES 5
 
 // Type definitions:
+
+//! \struct DisplayColors
+//! \brief Colors used when displaying text information on the terminal.
+//!
+//! When displaying messages or tags on the terminal, the Message Logger module
+//! must known the colors to use both for the text font and for the background.
+//! The colors used can vary in different message or tag types.
 typedef struct {
-  Color background_color;
-  Color text_color;
+  Color background_color;       //!< Color used for the text's background.
+  Color text_color;             //!< Color used for the text's font.
 } DisplayColors;
 
+//! \struct LoggerColorPallet
+//! \brief Display color information for all categories of messages and tags.
+//!
+//! This structure contains the display color information for each and every
+//! type of message and tag utilized by the Message Logger module. Access to
+//! information contained in the #message_colors and #tag_colors arrays should
+//! be done using the #MessageCategory and #TagCategory enumerations,
+//! respectively, as exemplified below:
+//! \n
+//! \code
+//! LoggerColorPallet color_pallet = {
+//!   // Assign color pallet members some values...
+//! };
+//! Color color1 = color_pallet.message_colors[DEFAULT_MSG].text_color;
+//! Color color2 = color_pallet.tag_colors[ERROR_TAG].background_color;
+//! \endcode
 typedef struct {
+  //! Display colors for all message categories in #MessageCategory.
   DisplayColors message_colors[NUM_OF_MESSAGE_CATEGORIES];
+  //! Display colors for all tag categories in #TagCategory.
   DisplayColors tag_colors[NUM_OF_TAG_CATEGORIES];
 } LoggerColorPallet;
 
+//! \struct TimeFormat
+//! \brief Time formatting information for storing messages in log files.
+//!
+//! When a log file is configured, the %TimeFormat information is necessary to
+//! determine how the message's time will be written into the log file.
 typedef struct {
+  //! String specifying the time format for logs. Max length is #TIME_FMT_SIZE.
   char string_representation[TIME_FMT_SIZE];
 } TimeFormat;
 
